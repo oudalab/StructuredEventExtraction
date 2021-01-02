901f8e68406bf557bdb7f8260125eaf36d3bcde1
Ubuntu <ubuntu@ip-172-31-41-145.ec2.internal>
2021-01-02 23:12:53 +0000

    update more inf

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config(object):
 	apr_dir = './model/'
 	data_dir = './data/'
 	model_name = 'model_1.pt'
-	epoch = 5
+	epoch = 2
 	bert_model = 'bert-base-cased'
 	lr = 5e-5
 	eps = 1e-8
@@ -11,5 +11,5 @@ class Config(object):
 	training_data = 'train_mavendata.txt'
 	val_data = 'dev_ref_mavendata.txt'
 	test_data = 'test_mavendata_documentid.txt'
-	test_out = 'test_prediction.csv'
+	test_out = 'test_prediction_id.csv'
 	raw_prediction_output = 'bert_prediction.csv'
